57c02f2fd75c75d33a9daf8eee05c6afc9909476
Erik Beckstrand <erik.beckstrand@gmail.com>
2019-10-05 10:40:55 -0600

    add firebase authentication?

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
 
-    var database = firebase.database();
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    function signIn() {
+
+    firebase.auth().signInWithPopup(provider)/*.then(function(result) {
+        console.log(result);
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });*/
+    };
+
+    signIn();
+
+
 
     var ingredients = [];
     var addIngredients = "";
